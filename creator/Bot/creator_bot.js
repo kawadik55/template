6f12d177a9cdbaaa1279a4c77e59a70696c1b171
Вадим <vadim@vadimkam.ru>
2025-10-24 16:53:17 +0500

    !

--- creator/Bot/creator_bot.js
+++ creator/Bot/creator_bot.js
@@ -4852,10 +4852,10 @@ function setContextFiles()
 		{	fs.copyFileSync(cBot+'/config.json',currentDir+'/config.json');
 		}
 		if(fs.existsSync(currentDir+'/config.json'))
-		{	let obj;
-			try{obj = require(currentDir+'/config.json');}catch(err){console.log(err);}
-			if(!Object.hasOwn(obj,'utcOffset')) {obj.utcOffset = utcOffset>0?'+'+String(moment().utcOffset()):String(moment().utcOffset()); WriteFileJson(currentDir+'/config.json',obj);}
-			if(!Object.hasOwn(obj,'community_text')) {obj.community_text='чистого времени'; WriteFileJson(currentDir+'/config.json',obj);}
+		{	//let obj;
+			//try{obj = require(currentDir+'/config.json');}catch(err){console.log(err);}
+			//if(!Object.hasOwn(obj,'utcOffset')) {obj.utcOffset = utcOffset>0?'+'+String(moment().utcOffset()):String(moment().utcOffset()); WriteFileJson(currentDir+'/config.json',obj);}
+			//if(!Object.hasOwn(obj,'community_text')) {obj.community_text='чистого времени'; WriteFileJson(currentDir+'/config.json',obj);}
 		}
 		//если запрошено изменение текста сообщества
 		if(!!COMMUNITY_TEXT_QW)
