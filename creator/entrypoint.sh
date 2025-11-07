a692bddf4e47eaf0d970bc952669d3401441b3c3
Вадим <vadim@vadimkam.ru>
2025-11-07 23:12:27 +0500

    test

--- creator/entrypoint.sh
+++ creator/entrypoint.sh
@@ -1,20 +1,10 @@
-echo "=== ENTRYPOINT STARTED ==="
-echo "HOST_UID: $HOST_UID"
-echo "Current UID: $(id -u)"
-echo "Current user: $(whoami)"
-
+#!/bin/bash
 # Если передан HOST_UID, меняем UID пользователя pi
 if [ -n "$HOST_UID" ] && [ "$HOST_UID" != "1000" ]; then
-    echo "Changing UID from 1000 to $HOST_UID"
-    sudo usermod -u $HOST_UID pi
-    sudo groupmod -g $HOST_UID pi
-    sudo chown -R pi:pi /home/pi
-    echo "New UID: $(id -u pi)"
-else
-    echo "No HOST_UID provided or already 1000"
+    usermod -u $HOST_UID pi
+    groupmod -g $HOST_UID pi
+    chown -R pi:pi /home/pi
 fi
 
-echo "=== ENTRYPOINT FINISHED ==="
-
-# Запускаем основную команду
-exec "$@"
+# Переключаемся на пользователя pi и запускаем бинарник
+exec su - pi -c "$@"
